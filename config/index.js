b0de9c73eff3c8473c47472ec072b05fb23f72bb
Dog-king8 <mlw1872824881@163.com>
2024-06-02 22:23:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxe5954b02675f47a5",
+  appsecret: "02b7b405b497df2bac2edc162e5893de",
+  template_id: "bCf5Y0bgdXh8R97N0Uxh2A91-_Z9qo-7PM5cU4EuLV8",
+  touser: "o4z906TBB6CS6MeVV6Q0wMKxYlhs",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "青岛",
+  first_date: "2023-10-12",
 };
